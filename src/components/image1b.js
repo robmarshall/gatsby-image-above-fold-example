04c39abee21e171bfb194a5477f62ba4c5bb14dd
robmarshall <robertmarshllproductions@gmail.com>
2020-07-03 14:24:36 +0100

    Corrected typo in srcSet

--- src/components/image1b.js
+++ src/components/image1b.js
@@ -1,17 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 const Image1b = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -29,16 +18,16 @@ const Image1b = () => {
     <picture>
       <source
         type="image/webp"
-        srcset={data.placeholderImage.childImageSharp.fluid.srcSetWebp}
+        srcSet={data.placeholderImage.childImageSharp.fluid.srcSetWebp}
         sizes={data.placeholderImage.childImageSharp.fluid.sizes}
       />
       <source
-        srcset={data.placeholderImage.childImageSharp.fluid.srcSet}
+        srcSet={data.placeholderImage.childImageSharp.fluid.srcSet}
         sizes={data.placeholderImage.childImageSharp.fluid.sizes}
       />
       <img
         sizes={data.placeholderImage.childImageSharp.fluid.sizes}
-        srcset={data.placeholderImage.childImageSharp.fluid.srcSet}
+        srcSet={data.placeholderImage.childImageSharp.fluid.srcSet}
         src={data.placeholderImage.childImageSharp.fluid.src}
         alt=""
       />
