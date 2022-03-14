82eb25c9eaa75b95da8916d9ec66d94d1282cb46
Robert <robertmarshallproductions@gmail.com>
2022-03-14 22:54:44 +0000

    Remove fade in

--- src/components/image1d.js
+++ src/components/image1d.js
@@ -2,7 +2,13 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
 const Image1d = () => (
-  <StaticImage src="../images/sunset.jpg" width={800} loading="eager" alt="" />
+  <StaticImage
+    src="../images/sunset.jpg"
+    width={800}
+    loading="eager"
+    fadeIn={false}
+    alt=""
+  />
 );
 
 export default Image1d;
